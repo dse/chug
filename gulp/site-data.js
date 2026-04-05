f928fddf05af607511edb24ba365d2d14e878922
Darren Embry <dse@webonastick.com>
2026-04-05 01:40:48 -0400

    add registerSiteData.

--- gulp/site-data.js
+++ gulp/site-data.js
@@ -3,6 +3,8 @@ import { glob } from "glob";
 
 import { EXCLUDE_PARTIALS, EXCLUDE_TEMP_FILES } from './constants.js';
 
+const registry = {};
+
 export function createSiteData(options) {
     const filenames = glob.sync([
         "src/data/**/*.json",
@@ -10,15 +12,21 @@ export function createSiteData(options) {
     ]);
     filenames.sort();
     const o = {};
+    Object.assign(o, registry);
     for (const filename of filenames) {
         const text = fs.readFileSync(filename, "utf-8");
         const obj = JSON.parse(text);
         Object.assign(o, obj);
     }
     Object.assign(o, {
-        "cacheBuster": String(new Date().getTime()) + "." + String(Math.floor(1 + Math.random())).slice(2)
+        "cacheBuster": (String(new Date().getTime()) + "." +
+                        String(Math.floor(1 + Math.random())).slice(2))
     });
     return o;
+}
+
+export function registerSiteData(name, value) {
+    registry[name] = value;
 }
 
 export default createSiteData;
