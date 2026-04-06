34fa764bb01e41094a6d7861064ee7299e39c116
Darren Embry <dse@webonastick.com>
2026-04-05 22:13:34 -0400

    allow additions to site data from gulpfile (otm/website/v5)

--- gulp/site-data.js
+++ gulp/site-data.js
@@ -3,6 +3,8 @@ import { glob } from "glob";
 
 import { EXCLUDE_PARTIALS, EXCLUDE_TEMP_FILES } from './constants.js';
 
+const registry = {};
+
 export function createSiteData(options) {
     const filenames = glob.sync([
         "src/data/**/*.json",
@@ -10,6 +12,7 @@ export function createSiteData(options) {
     ]);
     filenames.sort();
     const o = {};
+    Object.assign(o, registry);
     for (const filename of filenames) {
         const text = fs.readFileSync(filename, "utf-8");
         const obj = JSON.parse(text);
@@ -19,6 +22,10 @@ export function createSiteData(options) {
         "cacheBuster": String(new Date().getTime()) + "." + String(Math.floor(1 + Math.random())).slice(2)
     });
     return o;
+}
+
+export function registerSiteData(name, value) {
+    registry[name] = value;
 }
 
 export default createSiteData;
