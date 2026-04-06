5d9acc964868474acc263b2aca7f8a68fc0abb4d
Darren Embry <dse@webonastick.com>
2026-04-05 22:14:04 -0400

    allow separate watchtasks (e.g. don't import rollup if not npm installed)

--- gulp/watch.js
+++ gulp/watch.js
@@ -3,7 +3,7 @@ import gulp from 'gulp';
 import { EXCLUDE_TEMP_FILES, EXCLUDE_PARTIALS } from './constants.js';
 import { NUNJUCKS_PARTIALS } from './nunjucks.js';
 import { sassTask } from './sass.js';
-import { rollupTask } from './rollup.js';
+// import { rollupTask } from './rollup.js';
 import { nunjucksTask, resetLastRunTask } from './nunjucks.js';
 import { reloadTask } from './server.js';
 
@@ -15,8 +15,23 @@ export function watchTask() {
                gulp.series(resetLastRunTask, nunjucksTask, reloadTask));
     gulp.watch(['src/styles/**/*.scss', ...EXCLUDE_TEMP_FILES],
                gulp.series(sassTask, reloadTask));
-    gulp.watch(['src/scripts/**/*.js', ...EXCLUDE_TEMP_FILES],
-               gulp.series(rollupTask, reloadTask));
+    import("./rollup.js").then(({ rollupTask }) => {
+        gulp.watch(['src/scripts/**/*.js', ...EXCLUDE_TEMP_FILES],
+                   gulp.series(rollupTask, reloadTask));
+    });
+}
+
+export function watchNunjucksTask() {
+    console.info(`watchNunjucksTask: starting`);
+    gulp.watch(['src/pages/**/*.njk', ...EXCLUDE_TEMP_FILES, ...EXCLUDE_PARTIALS],
+               gulp.series(nunjucksTask, reloadTask));
+    gulp.watch([...NUNJUCKS_PARTIALS, ...EXCLUDE_TEMP_FILES],
+               gulp.series(resetLastRunTask, nunjucksTask, reloadTask));
+}
+export function watchSassTask() {
+    console.info(`watchStylesTask: starting`);
+    gulp.watch(['src/styles/**/*.scss', ...EXCLUDE_TEMP_FILES],
+               gulp.series(sassTask, reloadTask));
 }
 
 export default watchTask;
